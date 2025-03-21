bd89fe1dc8556eb29e69a6393b786ba1a206d21b
Saleem Kathat <playerkhan0001@gmail.com>
2025-03-21 22:28:54 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WordFlow',
+      title: 'RAEESKHAN 786',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const SplashScreen(),
     );
